817278ac7cf061fdeb683127f3346df3a1a4601c
Robertkill <lvpeilong@uniontech.com>
2024-04-03 15:19:19 +0800

    fix: Show desktop item is not displayed in the bottom right corner.  Modify the style and position of the show desktop item.
    log:
    issue: https://github.com/linuxdeepin/developer-center/issues/7696

--- panels/dock/showdesktop/showdesktop.h
+++ panels/dock/showdesktop/showdesktop.h
@@ -13,22 +13,13 @@ namespace dock {
 class ShowDesktop : public DApplet
 {
     Q_OBJECT
-    Q_PROPERTY(QString iconName READ iconName WRITE setIconName NOTIFY iconNameChanged FINAL)
 public:
     explicit ShowDesktop(QObject *parent = nullptr);
     virtual bool init() override;
     virtual bool load() override;
 
-    QString iconName() const;
-    void setIconName(const QString& iconName);
-
     Q_INVOKABLE void toggleShowDesktop();
-
-Q_SIGNALS:
-    void iconNameChanged();
-
-private:
-    QString m_iconName;
+    Q_INVOKABLE bool checkNeedShowDesktop();
 };
 
 }
