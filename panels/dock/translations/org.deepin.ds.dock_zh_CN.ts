18b8efb9fecfd5c9b3c3df7ae95f8a3b0723b987
zhaoyingzhen <zhaoyingzhen@uniontech.com>
2024-04-09 18:12:17 +0800

    feat: add translation files
    Add translation files
    
    Log: add translation files
    Influence: translation

---
+++ panels/dock/translations/org.deepin.ds.dock_zh_CN.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../package/main.qml" line="112"/>
+        <source>Indicator Style</source>
+        <translation>指示器样式</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="114"/>
+        <source>Fashion Mode</source>
+        <translation>时尚模式</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="119"/>
+        <source>Efficient Mode</source>
+        <translation>高效模式</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="125"/>
+        <source>Item Alignment</source>
+        <translation>对齐方式</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="128"/>
+        <location filename="../package/main.qml" line="154"/>
+        <source>Left</source>
+        <translation>左</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="133"/>
+        <source>Center</source>
+        <translation>居中</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="139"/>
+        <source>Position</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="143"/>
+        <source>Top</source>
+        <translation>顶部</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="148"/>
+        <source>Bottom</source>
+        <translation>底部</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="160"/>
+        <source>Right</source>
+        <translation>右</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="166"/>
+        <source>Status</source>
+        <translation>状态</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="168"/>
+        <source>Keep Shown</source>
+        <translation>一直显示</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="173"/>
+        <source>Keep Hidden</source>
+        <translation>一直隐藏</translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="179"/>
+        <source>Smart Hide</source>
+        <translation>智能隐藏</translation>
+    </message>
+</context>
+</TS>
---
+++ panels/osd/displaymode/translations/org.deepin.ds.osd.displaymode.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>osd::DisPlayModeApplet</name>
+    <message>
+        <location filename="../displaymodeapplet.cpp" line="152"/>
+        <source>Duplicate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaymodeapplet.cpp" line="153"/>
+        <source>Extend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../displaymodeapplet.cpp" line="158"/>
+        <source>Only on %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
