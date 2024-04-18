3a3a8d24ced47e8c6421f4d78e16de242aa27d72
tsic404 <liuheng@deepin.org>
2024-04-18 09:46:49 +0800

    fix: rename mode to Alignment
    update menu text and translate
    
    log: as title
    issue: https://github.com/linuxdeepin/developer-center/issues/7955

--- panels/dock/translations/org.deepin.ds.dock_az.ts
+++ panels/dock/translations/org.deepin.ds.dock_az.ts
@@ -4,72 +4,82 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../package/main.qml" line="112"/>
+        <location filename="../package/main.qml" line="148"/>
         <source>Indicator Style</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="133"/>
+        <location filename="../package/main.qml" line="150"/>
         <source>Fashion Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="128"/>
+        <location filename="../package/main.qml" line="155"/>
         <source>Efficient Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="154"/>
+        <location filename="../package/main.qml" line="161"/>
+        <source>Alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="189"/>
         <source>Left</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="126"/>
-        <source>Mode</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../package/main.qml" line="139"/>
+        <location filename="../package/main.qml" line="174"/>
         <source>Position</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="143"/>
-        <source>Top</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../package/main.qml" line="148"/>
-        <source>Bottom</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../package/main.qml" line="160"/>
-        <source>Right</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../package/main.qml" line="166"/>
-        <source>Status</source>
+        <location filename="../package/main.qml" line="163"/>
+        <source>Align Left</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../package/main.qml" line="168"/>
+        <source>Align Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="178"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="183"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="195"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="201"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="203"/>
         <source>Keep Shown</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="173"/>
+        <location filename="../package/main.qml" line="208"/>
         <source>Keep Hidden</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="179"/>
+        <location filename="../package/main.qml" line="214"/>
         <source>Smart Hide</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../package/main.qml" line="186"/>
+        <location filename="../package/main.qml" line="221"/>
         <source>Dock Settings</source>
         <translation type="unfinished"></translation>
     </message>
