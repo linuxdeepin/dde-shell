5f874cc00e6eac3086ec7d6085bc89b5275b7ac7
Ye ShanShan <yeshanshan@uniontech.com>
2024-06-05 14:40:15 +0800

    feat: add dquickdrag for animation drag
    it only works in x11,
    in wayland, we don't know window's position to animation.
    DragOverlayWinow's position is also incorrect.

---
+++ frame/quick/dsquickdrag.cpp
@@ -0,0 +1,275 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "dsquickdrag_p.h"
+
+#include <QLoggingCategory>
+#include <QCoreApplication>
+#include <QPointer>
+#include <private/qguiapplication_p.h>
+#include <qpa/qplatformintegration.h>
+#include <qpa/qplatformdrag.h>
+#define protected public
+#include <private/qsimpledrag_p.h>
+#undef protected
+
+#include <dobject_p.h>
+
+DS_BEGIN_NAMESPACE
+
+Q_LOGGING_CATEGORY(dsDragLog, "dde.shell.drag")
+
+class DragWindowEventFilter : public QObject {
+public:
+    explicit DragWindowEventFilter(DQuickDragPrivate *drag)
+        : m_drag(drag)
+    {
+    }
+    ~DragWindowEventFilter() override
+    {
+    }
+
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+private:
+    QWindow *dragWindow(QObject *object) const
+    {
+        auto platformDrag = QGuiApplicationPrivate::platformIntegration()->drag();
+        if (platformDrag && platformDrag->ownsDragObject()) {
+            if (auto drag = dynamic_cast<QBasicDrag *>(platformDrag)) {
+                if (object == reinterpret_cast<QObject *>(drag->shapedPixmapWindow()))
+                    return qobject_cast<QWindow *>(object);
+            }
+        }
+        return nullptr;
+    }
+    DQuickDragPrivate *m_drag {nullptr};
+};
+
+class DQuickDragPrivate : public DTK_CORE_NAMESPACE::DObjectPrivate
+{
+public:
+    D_DECLARE_PUBLIC(DQuickDrag)
+    explicit DQuickDragPrivate(DQuickDrag *qq);
+
+    void initDragOverlay(QWindow *dragWindow);
+    void updateOverlayPosition(const QPoint &pos);
+    void showOverlay();
+    void hideOverlay();
+
+private:
+    QWindow *createDragOverlay();
+    void updateStartDragPoint();
+    void updateCurrentDragPoint();
+    QPoint hotSpot() const;
+
+    QPointer<QWindow> m_window {nullptr};
+    QPointer<DragWindowEventFilter> m_filter;
+    bool m_active {false};
+    QSizeF m_hotSpotScale {};
+    QPoint m_startDragPoint {0, 0};
+    QPoint m_currentDragPoint {0, 0};
+    QQmlComponent *m_overlay = nullptr;
+};
+
+bool DragWindowEventFilter::eventFilter(QObject *watched, QEvent *event)
+{
+    auto dragWindow = this->dragWindow(watched);
+    if (!dragWindow)
+        return false;
+
+    if (event->type() == QEvent::PlatformSurface) {
+        qDebug(dsDragLog) << "drag window platform surface";
+        m_drag->initDragOverlay(dragWindow);
+    }
+
+    if (event->type() == QEvent::Move) {
+        m_drag->updateOverlayPosition(dragWindow->position());
+    }
+
+    if (event->type() == QEvent::Show) {
+        qDebug(dsDragLog) << "drag window show";
+        m_drag->showOverlay();
+    }
+    if (event->type() == QEvent::Hide) {
+        qDebug(dsDragLog) << "drag window hide";
+        m_drag->hideOverlay();
+    }
+    return false;
+}
+
+DQuickDrag::DQuickDrag(QObject *parent)
+    : QObject(parent)
+    , DObject(*new DQuickDragPrivate(this))
+{
+}
+
+DQuickDrag *DQuickDrag::qmlAttachedProperties(QObject *object)
+{
+    return new DQuickDrag(object);
+}
+
+bool DQuickDrag::active() const
+{
+    D_DC(DQuickDrag);
+    return d->m_active;
+}
+
+void DQuickDrag::setActive(bool newActive)
+{
+    D_D(DQuickDrag);
+    if (d->m_active == newActive) {
+        return;
+    }
+
+    if (d->m_filter) {
+        qApp->removeEventFilter(d->m_filter);
+        d->m_filter->deleteLater();
+        d->m_window->deleteLater();
+    }
+
+    if (newActive) {
+        d->m_filter = new DragWindowEventFilter(d);
+        qApp->installEventFilter(d->m_filter);
+    }
+
+    d->m_active = newActive;
+    emit activeChanged();
+}
+
+QPoint DQuickDrag::startDragPoint() const
+{
+    D_DC(DQuickDrag);
+    return d->m_startDragPoint;
+}
+
+QQmlComponent *DQuickDrag::overlay() const
+{
+    D_DC(DQuickDrag);
+    return d->m_overlay;
+}
+
+void DQuickDrag::setOverlay(QQmlComponent *newOverlay)
+{
+    D_D(DQuickDrag);
+    if (d->m_overlay == newOverlay)
+        return;
+    d->m_overlay = newOverlay;
+    emit overlayChanged();
+}
+
+QSizeF DQuickDrag::hotSpotScale() const
+{
+    D_DC(DQuickDrag);
+    return d->m_hotSpotScale;
+}
+
+void DQuickDrag::setHotSpotScale(const QSizeF &newHotSpotScale)
+{
+    D_D(DQuickDrag);
+    if (d->m_hotSpotScale == newHotSpotScale)
+        return;
+    d->m_hotSpotScale = newHotSpotScale;
+    emit hotSpotScaleChanged();
+}
+
+void DQuickDrag::resetHotSpotScale()
+{
+    D_D(DQuickDrag);
+    d->m_hotSpotScale = {};
+    emit hotSpotScaleChanged();
+}
+
+QPoint DQuickDrag::currentDragPoint() const
+{
+    D_DC(DQuickDrag);
+    return d->m_currentDragPoint;
+}
+
+DQuickDragPrivate::DQuickDragPrivate(DQuickDrag *qq)
+    : DTK_CORE_NAMESPACE::DObjectPrivate(qq)
+{
+}
+
+void DQuickDragPrivate::showOverlay()
+{
+    if (!m_window)
+        return;
+    m_window->show();
+}
+
+void DQuickDragPrivate::hideOverlay()
+{
+    if (!m_window)
+        return;
+    m_window->hide();
+}
+
+QWindow *DQuickDragPrivate::createDragOverlay()
+{
+    if (!m_overlay)
+        return nullptr;
+
+    auto object = m_overlay->create(m_overlay->creationContext());
+    if (!object) {
+        qWarning(dsDragLog) << "Failed to create overlay" << m_overlay->errorString();
+    }
+    return qobject_cast<QWindow *>(object);
+}
+
+void DQuickDragPrivate::initDragOverlay(QWindow *dragWindow)
+{
+    if (!m_window)
+        m_window = createDragOverlay();
+
+    if (!m_window)
+        return;
+
+    qDebug(dsDragLog) << "init drag overlay" << m_window;
+
+    m_window->setTransientParent(dragWindow);
+
+    updateOverlayPosition(dragWindow->position());
+    updateStartDragPoint();
+}
+
+void DQuickDragPrivate::updateOverlayPosition(const QPoint &pos)
+{
+    if (!m_window)
+        return;
+
+    m_window->setPosition(pos - hotSpot());
+    updateCurrentDragPoint();
+}
+
+void DQuickDragPrivate::updateStartDragPoint()
+{
+    if (!m_window)
+        return;
+
+    const auto pos = m_window->position();
+    m_startDragPoint = pos - hotSpot();
+
+    D_Q(DQuickDrag);
+    emit q->startDragPointChanged();
+}
+
+void DQuickDragPrivate::updateCurrentDragPoint()
+{
+    m_currentDragPoint = m_window->position();
+    D_Q(DQuickDrag);
+    emit q->currentDragPointChanged();
+}
+
+QPoint DQuickDragPrivate::hotSpot() const
+{
+    if (!m_window || !m_hotSpotScale.isValid())
+        return {};
+
+    const auto x = m_window->width() * m_hotSpotScale.width();
+    const auto y = m_window->height() * m_hotSpotScale.height();
+    return QPoint(x, y);
+}
+
+DS_END_NAMESPACE
---
+++ frame/quick/dsquickdrag_p.h
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "dsglobal.h"
+
+#include <QQmlComponent>
+#include <QObject>
+#include <DObject>
+
+DS_BEGIN_NAMESPACE
+
+class DQuickDragPrivate;
+class DQuickDrag : public QObject, public DTK_CORE_NAMESPACE::DObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool active READ active WRITE setActive NOTIFY activeChanged FINAL)
+    Q_PROPERTY(QQmlComponent* overlay READ overlay WRITE setOverlay NOTIFY overlayChanged FINAL)
+    Q_PROPERTY(QSizeF hotSpotScale READ hotSpotScale WRITE setHotSpotScale RESET resetHotSpotScale NOTIFY hotSpotScaleChanged FINAL)
+    Q_PROPERTY(QPoint startDragPoint READ startDragPoint NOTIFY startDragPointChanged FINAL)
+    Q_PROPERTY(QPoint currentDragPoint READ currentDragPoint NOTIFY currentDragPointChanged FINAL)
+    D_DECLARE_PRIVATE(DQuickDrag)
+public:
+    explicit DQuickDrag(QObject *parent = nullptr);
+
+    bool active() const;
+    void setActive(bool newActive);
+
+    QQmlComponent *overlay() const;
+    void setOverlay(QQmlComponent *newOverlay);
+
+    QSizeF hotSpotScale() const;
+    void setHotSpotScale(const QSizeF &newHotSpot);
+    void resetHotSpotScale();
+
+    QPoint startDragPoint() const;
+    QPoint currentDragPoint() const;
+
+    static DQuickDrag *qmlAttachedProperties(QObject *object);
+signals:
+    void activeChanged();
+    void startDragPointChanged();
+    void overlayChanged();
+    void hotSpotScaleChanged();
+    void currentDragPointChanged();
+};
+
+DS_END_NAMESPACE
+
+QML_DECLARE_TYPEINFO(DS_NAMESPACE::DQuickDrag, QML_HAS_ATTACHED_PROPERTIES)
