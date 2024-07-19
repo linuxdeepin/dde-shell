9ca6e3e709659e95401266d64fafa4ffb6b44048
swq <shenwenqi@uniontech.com>
2024-07-19 17:00:28 +0800

    fix: Update taskbar plugin translation
    Issue: linuxdeepin/developer-center#9671

---
+++ panels/dock/tray/translations/org.deepin.ds.dock.tray_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_AS">
+<context>
+    <name>PanelTrayItem</name>
+    <message>
+        <location filename="../quickpanel/PanelTrayItem.qml" line="27"/>
+        <source>Quick actions</source>
+        <translation>Quick actions</translation>
+    </message>
+</context>
+</TS>
