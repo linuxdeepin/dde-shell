d8963cab157671f1f97a04d3b5b2222c3dca4b00
zhaoyingzhen <zhaoyingzhen@uniontech.com>
2024-12-04 10:24:17 +0800

    chore: TrayLoader single process support to start multiple plugins
    1.Divide plugins into three groups(selfMaintenance,application,other)
    2.single process support to start multiple plugins
    
    Log: as title
    Bug: https://pms.uniontech.com/bug-view-283901.html

--- panels/dock/loadtrayplugins.cpp
+++ panels/dock/loadtrayplugins.cpp
@@ -7,6 +7,8 @@
 
 #include <signal.h>
 
+#include <DConfig>
+
 #include <QDir>
 #include <QTimer>
 #include <QGuiApplication>
@@ -32,58 +34,17 @@ LoadTrayPlugins::~LoadTrayPlugins()
 
 void LoadTrayPlugins::loadDockPlugins()
 {
-    QStringList filters;
-    filters << "*.so";
-
-    QStringList execPaths;
-    execPaths << qEnvironmentVariable("TRAY_LOADER_EXECUTE_PATH")
-              << QString("%1/trayplugin-loader").arg(CMAKE_INSTALL_FULL_LIBEXECDIR);
-
-    QString validExePath;
-    for (const QString &execPath : execPaths) {
-        if (QFile::exists(execPath)) {
-            validExePath = execPath;
-            break;
-        }
-    }
-
+    QString validExePath = loaderPath();
     if (validExePath.isEmpty()) {
         qWarning() << "No valid loader executable path found.";
         return;
     }
 
-    QStringList dirs;
-    const auto pluginsPath = qEnvironmentVariable("TRAY_DEBUG_PLUGIN_PATH");
-    if (!pluginsPath.isEmpty())
-        dirs << pluginsPath.split(QDir::listSeparator());
-
-    if (dirs.isEmpty())
-        dirs << pluginDirs;
-
-    for (auto &pluginDir : dirs) {
-        QDir dir(pluginDir);
-        if (!dir.exists()) {
-            qWarning() << "The plugin directory does not exist:" << pluginDir;
-            continue;
-        }
-
-        auto pluginFileInfos = dir.entryInfoList(filters, QDir::Files);
-        foreach (auto pluginInfo, pluginFileInfos) {
-            qInfo() << "pluginLoader load plugin" << pluginInfo.absoluteFilePath();
-
-            QProcess *process = new QProcess(this);
-            setProcessEnv(process);
-
-            connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
-                    this, &LoadTrayPlugins::handleProcessFinished);
-
-            ProcessInfo pInfo = { process, pluginInfo.absoluteFilePath(), 0 };
-            m_processes.append(pInfo);
-
-            process->setProgram(validExePath);
-            process->setArguments({"-p", pluginInfo.absoluteFilePath(), "-platform", "wayland"});
-            process->start();
-        }
+    auto pluginGroupMap = groupPlugins(allPluginPaths());
+    for (auto it = pluginGroupMap.begin(); it != pluginGroupMap.end(); ++it) {
+        if (it.value().isEmpty()) continue;
+        qDebug() << "Load plugin:" << it.value() << " group:" << it.key();
+        startProcess(validExePath, it.value(), it.key());
     }
 }
 
@@ -113,6 +74,22 @@ void LoadTrayPlugins::handleProcessFinished(int exitCode, QProcess::ExitStatus e
     }
 }
 
+void LoadTrayPlugins::startProcess(const QString &loaderPath, const QString &pluginPath, const QString &groupName)
+{
+    auto *process = new QProcess(this);
+    setProcessEnv(process);
+
+    connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+            this, &LoadTrayPlugins::handleProcessFinished);
+
+    ProcessInfo pInfo = { process, pluginPath, 0 };
+    m_processes.append(pInfo);
+
+    process->setProgram(loaderPath);
+    process->setArguments({"-p", pluginPath, "-g", groupName, "-platform", "wayland"});
+    process->start();
+}
+
 void LoadTrayPlugins::setProcessEnv(QProcess *process)
 {
     if (!process) return;
@@ -124,4 +101,99 @@ void LoadTrayPlugins::setProcessEnv(QProcess *process)
     process->setProcessEnvironment(env);
 }
 
+QString LoadTrayPlugins::loaderPath() const
+{
+    QStringList execPaths;
+    execPaths << qEnvironmentVariable("TRAY_LOADER_EXECUTE_PATH")
+              << QString("%1/trayplugin-loader").arg(CMAKE_INSTALL_FULL_LIBEXECDIR);
+
+    QString validExePath;
+    for (const QString &execPath : execPaths) {
+        if (QFile::exists(execPath)) {
+            validExePath = execPath;
+            break;
+        }
+    }
+
+    return validExePath;
+}
+
+QStringList LoadTrayPlugins::allPluginPaths() const
+{
+    QStringList dirs;
+    const auto pluginsPath = qEnvironmentVariable("TRAY_DEBUG_PLUGIN_PATH");
+    if (!pluginsPath.isEmpty())
+        dirs << pluginsPath.split(QDir::listSeparator());
+
+    if (dirs.isEmpty())
+        dirs << pluginDirs;
+
+    QStringList pluginPaths;
+    for (auto &pluginDir : dirs) {
+        QDir dir(pluginDir);
+        if (!dir.exists()) {
+            qWarning() << "The plugin directory does not exist:" << pluginDir;
+            continue;
+        }
+
+        auto pluginFileInfos = dir.entryInfoList({"*.so"}, QDir::Files);
+        for (auto &pluginInfo : pluginFileInfos) {
+            pluginPaths.append(pluginInfo.absoluteFilePath());
+        }
+    }
+
+    return pluginPaths;
+}
+
+QMap<QString, QString> LoadTrayPlugins::groupPlugins(const QStringList &pluginPaths) const
+{
+    const QString selfMaintenancePluginsKey = "selfMaintenanceTrayPlugins";
+    const QString subprojectPluginsKey = "subprojectTrayPlugins";
+    const QString crashPronePluginsKey = "crashProneTrayPlugins";
+    const QString otherPluginsKey = "otherTrayPlugins";
+
+    auto dConfig = Dtk::Core::DConfig::create("org.deepin.dde.shell", "org.deepin.ds.dock.tray", QString());
+    QStringList selfMaintenanceTrayPlugins = dConfig->value(selfMaintenancePluginsKey).toStringList();
+    QStringList subprojectTrayPlugins = dConfig->value(subprojectPluginsKey).toStringList();
+    QStringList crashProneTrayPlugins = dConfig->value(crashPronePluginsKey).toStringList();
+    dConfig->deleteLater();
+
+    QStringList selfMaintenancePluginPaths;
+    QStringList subprojectPluginPaths;
+    QStringList crashPronePluginPaths;
+    QStringList otherPluginPaths;
+
+    for (auto &filePath : pluginPaths) {
+        QString pluginName = filePath.section("/", -1);
+        if (crashProneTrayPlugins.contains(pluginName)) {
+            crashPronePluginPaths.append(filePath);
+        } else if (selfMaintenanceTrayPlugins.contains(pluginName)) {
+            selfMaintenancePluginPaths.append(filePath);
+        } else if (subprojectTrayPlugins.contains(pluginName)) {
+            subprojectPluginPaths.append(filePath);
+        } else {
+            otherPluginPaths.append(filePath);
+        }
+    }
+
+    QMap<QString, QString> pluginGroup;
+
+    if (!selfMaintenancePluginPaths.isEmpty()) {
+        pluginGroup.insert(selfMaintenancePluginsKey, selfMaintenancePluginPaths.join(";"));
+    }
+
+    if (!subprojectPluginPaths.isEmpty()) {
+        pluginGroup.insert(subprojectPluginsKey, subprojectPluginPaths.join(";"));
+    }
+
+    if (!crashPronePluginPaths.isEmpty()) {
+        pluginGroup.insert(crashPronePluginsKey, crashPronePluginPaths.join(";"));
+    }
+
+    if (!otherPluginPaths.isEmpty()) {
+        pluginGroup.insert(otherPluginsKey, otherPluginPaths.join(";"));
+    }
+
+    return pluginGroup;
+}
 }
