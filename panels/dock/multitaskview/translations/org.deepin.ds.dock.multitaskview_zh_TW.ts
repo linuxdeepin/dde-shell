95370f07e290d1ceb50429d92356172a31baf83e
Wang Zichong <wangzichong@deepin.org>
2025-07-30 14:39:47 +0800

    fix: add translation resources to config for multitasking view
    为多任务视图插件添加翻译资源文件.
    
    PMS: BUG-326855
    Log:

--- panels/dock/multitaskview/translations/org.deepin.ds.dock.multitaskview_zh_TW.ts
+++ panels/dock/multitaskview/translations/org.deepin.ds.dock.multitaskview_zh_TW.ts
@@ -1,18 +1,18 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
-    <name>dock::MultiTaskView</name>
-    <message>
-        <source>Multitasking View</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>multitaskview</name>
-    <message>
-        <source>Multitasking View</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
+<TS language="zh_TW" version="2.1">
+    <context>
+        <name>dock::MultiTaskView</name>
+        <message>
+            <source>Multitasking View</source>
+            <translation>多工檢視</translation>
+        </message>
+    </context>
+    <context>
+        <name>multitaskview</name>
+        <message>
+            <source>Multitasking View</source>
+            <translation>多工檢視</translation>
+        </message>
+    </context>
 </TS>
