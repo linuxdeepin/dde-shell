1b57ebcac7680b02566b27acfe78e7d48959105e
Wang Zichong <wangzichong@deepin.org>
2025-11-13 11:51:01 +0800

    chore: make hover preview proxy model simpler
    简化 HoverPreviewProxyModel, 使 model 本身不与设置项耦合, 过滤规则
    在 taskmanager.cpp 控制
    
    Log:

--- panels/dock/taskmanager/hoverpreviewproxymodel.cpp
+++ panels/dock/taskmanager/hoverpreviewproxymodel.cpp
@@ -4,7 +4,6 @@
 
 #include "hoverpreviewproxymodel.h"
 #include "taskmanager.h"
-#include "taskmanagersettings.h"
 
 #include <QDebug>
 
@@ -13,26 +12,22 @@ namespace dock
 
 HoverPreviewProxyModel::HoverPreviewProxyModel(QObject *parent)
     : QSortFilterProxyModel(parent)
-    , m_settings(TaskManagerSettings::instance())
 {
     // 设置动态排序，确保模型变化时自动重新过滤
     setDynamicSortFilter(true);
 }
 
-void HoverPreviewProxyModel::setFilterModelIndex(const QModelIndex &index)
+void HoverPreviewProxyModel::setFilter(QString filter, enum FilterMode mode)
 {
-    if (m_filterIndex == index)
-        return;
+    m_filter = filter;
+    m_filterMode = mode;
 
-    m_filterIndex = index;
-
-    // 触发过滤器重新计算
     invalidateFilter();
 }
 
 void HoverPreviewProxyModel::clearFilter()
 {
-    setFilterModelIndex(QModelIndex());
+    setFilter(QString(), FilterByAppId);
 }
 
 bool HoverPreviewProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
@@ -40,7 +35,7 @@ bool HoverPreviewProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex &
     Q_UNUSED(sourceParent)
 
     // 如果没有设置过滤条件，则不显示任何行
-    if (!m_filterIndex.isValid())
+    if (m_filter.isEmpty())
         return false;
 
     if (!sourceModel())
@@ -56,27 +51,19 @@ bool HoverPreviewProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex &
     if (winId == 0)
         return false;
 
-    // 根据 noTaskGrouping 配置采用不同的过滤策略
-    bool isWindowSplit = (m_settings && m_settings->isWindowSplit());
-
-    if (isWindowSplit) {
-        // noTaskGrouping = true: 精确匹配该模型索引对应的窗口
-        // 比较窗口ID是否匹配
-        QVariant filterWinId = m_filterIndex.data(TaskManager::WinIdRole);
-        uint32_t targetWinId = filterWinId.toUInt();
-
+    switch (m_filterMode) {
+    case FilterByAppId: {
+        QString currentDesktopId = sourceIndex.data(TaskManager::DesktopIdRole).toString();
+        return currentDesktopId == m_filter;
+    }
+    case FilterByWinId: {
+        uint32_t targetWinId = m_filter.toUInt();
         bool result = (winId == targetWinId && targetWinId != 0);
-        // 在 WindowSplit 模式下，精确匹配单个窗口
-        return result;
-    } else {
-        // noTaskGrouping = false: 基于 DesktopIdRole 匹配应用的所有窗口
-        QVariant currentDesktopId = sourceIndex.data(TaskManager::DesktopIdRole);
-        QVariant filterDesktopId = m_filterIndex.data(TaskManager::DesktopIdRole);
-
-        bool result = (currentDesktopId.toString() == filterDesktopId.toString());
-        // 在 Grouped 模式下，匹配同一应用的所有窗口
         return result;
     }
+    }
+
+    return false;
 }
 
 }
