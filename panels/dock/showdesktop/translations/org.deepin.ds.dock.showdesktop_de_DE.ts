cefb56f76dfc9231380b8fc4ba61c99ba380c86b
Wang Zichong <wangzichong@deepin.org>
2025-11-19 11:05:19 +0800

    i18n: remove de_DE translation resources
    移除 de_DE 翻译资源,仅保留 de (不带地区变体后缀)的资源.
    
    Log:

--- panels/dock/showdesktop/translations/org.deepin.ds.dock.showdesktop_de_DE.ts
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="de_DE">
-<context>
-    <name>showdesktop</name>
-    <message>
-        <source>show desktop</source>
-        <translation>Schreibtisch zeigen</translation>
-    </message>
-</context>
-</TS>
